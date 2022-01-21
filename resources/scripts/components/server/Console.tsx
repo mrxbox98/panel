171a9d18fc3724cae5cd6751b55544f56e798acf
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2022-01-20 21:44:40 -0800

    Will this help?

--- resources/scripts/components/server/Console.tsx
+++ resources/scripts/components/server/Console.tsx
@@ -216,7 +216,7 @@ export default () => {
                 ]}
                 style={{ minHeight: '16rem' }}
             >
-                <TerminalDiv css={window.location.href.includes("/console") ? tw`h-full` : tw``} id={'terminal'} ref={ref} />
+                <TerminalDiv css={window.location.href.includes("/console") ? tw`h-max` : tw``} id={'terminal'} ref={ref} />
             </div>
             {canSendCommands &&
                 <div css={tw`rounded-b bg-neutral-900 text-neutral-100 flex items-baseline`}>
