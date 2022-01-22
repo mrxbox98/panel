8e64b8741154f0e4fc072f64b397705a62a92517
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2022-01-21 17:16:24 -0800

    Shrink command input

--- resources/scripts/components/server/Console.tsx
+++ resources/scripts/components/server/Console.tsx
@@ -228,6 +228,7 @@ export default () => {
                             aria-label={'Console command input.'}
                             disabled={!instance || !connected}
                             onKeyDown={handleCommandKeyDown}
+                            style={{height: '90%'}}
                         />
                         <button hidden={window.location.href.includes("/console")} onClick={openConsole}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
