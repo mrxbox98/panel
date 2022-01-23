f28c187576bfd2a9c8275e73dd21aaf33f2afc77
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2022-01-23 12:05:23 -0800

    Change window size

--- resources/scripts/components/server/Console.tsx
+++ resources/scripts/components/server/Console.tsx
@@ -203,7 +203,7 @@ export default () => {
     }, [connected, instance]);
 
     function openConsole () {
-        window.open(window.location.href + "/console", "Server Console","height=500,width=1000");
+        window.open(window.location.href + "/console", "Server Console","height=100,width=1500");
     }
 
     return (
